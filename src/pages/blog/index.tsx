1bbb00d16c90ca98e8ee512822b79dc865caddbf
unstriped <stefanmagnusson90@gmail.com>
2023-08-29 22:39:28 +0200

    Dynamic Images

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -4,35 +4,26 @@ import BaseSeo from "../../components/baseSeo";
 import { HeadFC, PageProps, type StaticQueryDocument, graphql } from "gatsby";
 import BaseLink from "../../components/baseLink";
 
-export type mdxNode = {
-  frontmatter: {
-    date: string;
-    title: string;
-    slug: string;
-  };
-  id: string;
-  excerpt: string;
-};
-
 interface DataProps {
   allMdx: {
-    nodes: mdxNode[];
+    nodes: Queries.Mdx[];
   };
 }
 
 const BlogPage: React.FC<PageProps<DataProps>> = ({ data }) => {
-  console.log(data);
   return (
     <Layout pageTitle="My Blog Posts">
       {data.allMdx.nodes.map((node) => (
         <article className="my-4 max-w-lg" key={node.id}>
           <h2 className="font-semibold text-lg capitalize">
             <BaseLink
-              label={node.frontmatter.title}
-              to={node.frontmatter.slug}
+              label={node?.frontmatter?.title ?? ""}
+              to={node?.frontmatter?.slug || "/"}
             />
           </h2>
-          <p className="font-light italic">Posted: {node.frontmatter.date} </p>
+          <p className="font-light italic">
+            Posted: {node?.frontmatter?.date || "Unknown Date"}{" "}
+          </p>
           <p>{node.excerpt}</p>
         </article>
       ))}
--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -2,10 +2,15 @@ import * as React from "react";
 import Layout from "../../components/layout";
 import BaseSeo from "../../components/baseSeo";
 import { HeadFC, PageProps, type StaticQueryDocument, graphql } from "gatsby";
-import { mdxNode } from "./index";
+import {
+  GatsbyImage,
+  IGatsbyImageData,
+  ImageDataLike,
+  getImage,
+} from "gatsby-plugin-image";
 
 interface DataProps {
-  mdx: mdxNode;
+  mdx: Queries.Mdx;
 }
 
 const BlogPost: React.FC<PageProps<DataProps>> = ({
@@ -15,9 +20,31 @@ const BlogPost: React.FC<PageProps<DataProps>> = ({
   data: DataProps;
   children: undefined;
 }) => {
+  let image: IGatsbyImageData | undefined;
+  // Not sure how to fix this error, so using any for now
+  const img: any = data?.mdx?.frontmatter?.hero_image;
+  image = getImage(img);
+
   return (
-    <Layout pageTitle={data.mdx.frontmatter.title}>
-      <p className="italic">{data.mdx.frontmatter.date}</p>
+    <Layout pageTitle={data?.mdx?.frontmatter?.title || ""}>
+      <p className="italic">{data?.mdx?.frontmatter?.date || "Unknown Date"}</p>
+      {image && (
+        <>
+          <GatsbyImage
+            image={image}
+            alt={
+              data?.mdx?.frontmatter?.hero_image_alt || "Unknown Description"
+            }
+          />
+          <p className="text-slate-500 mb-4 mx-2">
+            Photo Credit:{" "}
+            <a href={data.mdx.frontmatter?.hero_image_credit_link || ""}>
+              {data?.mdx?.frontmatter?.hero_image_credit_text}
+            </a>
+          </p>
+        </>
+      )}
+
       {children}
     </Layout>
   );
@@ -28,14 +55,22 @@ export const query: StaticQueryDocument = graphql`
     mdx(id: { eq: $id }) {
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   }
 `;
 
 export const Head: HeadFC<DataProps> = ({ data }) => (
-  <BaseSeo title={data.mdx.frontmatter.title} />
+  <BaseSeo title={data?.mdx?.frontmatter?.title || ""} />
 );
 
 export default BlogPost;
